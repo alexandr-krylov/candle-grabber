f9867e5a7219363359315239f71017704df6977b
Alexandr Krylov <alexandr.krylov@gmail.com>
2024-02-13 00:48:48 +0600

    try to send requests and get candles

--- src/index.php
+++ src/index.php
@@ -4,10 +4,17 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use GuzzleHttp\Client;
 
-$baseUri = 'https://hitbtc.com/api/';
+$baseUri = 'https://api.hitbtc.com/api/3/';
 $timeout = 2;
-
 $client = new Client([
     'base_uri' => $baseUri,
     'timeout' => $timeout,
 ]);
+$params = ['query' => [
+    'period' => 'M1',
+    'from' => '2024-02-12T00:00:00Z',
+    'till' => '2024-02-12T00:15:00Z'
+]];
+$response = $client->get('public/candles/BTCUSDT', $params);
+$body = $response->getBody();
+echo $body;

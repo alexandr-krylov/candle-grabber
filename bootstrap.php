bce1b84f82db8102c3a24aa455f2df4c03c55ed2
Alexandr Krylov <alexandr.krylov@gmail.com>
2024-02-21 15:08:43 +0600

    doctrine bin added

---
+++ bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+
+use Doctrine\DBAL\DriverManager;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\ORMSetup;
+
+require_once 'vendor/autoload.php';
+
+$config = ORMSetup::createAttributeMetadataConfiguration(
+    paths: [__DIR__ . '/src'],
+    isDevMode: true
+);
+$connection = DriverManager::getConnection([
+    'driver' => 'pdo_sqlite',
+    'path' => __DIR__ . '/db.sqlite',
+], $config);
+$entityManager = new EntityManager($connection, $config);

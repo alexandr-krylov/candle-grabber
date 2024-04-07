52d43c287a214b0b36a81b0fba90a9ca42e89bf2
Alexandr Krylov <alexandr.krylov@gmail.com>
2024-04-07 19:12:31 +0500

    setup db container, setup xml config and db config

---
+++ bootstrap_xml_mariadb.php
@@ -0,0 +1,25 @@
+<?php
+
+use Doctrine\DBAL\DriverManager;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\ORMSetup;
+
+require_once 'vendor/autoload.php';
+
+$config = ORMSetup::createXMLMetadataConfiguration(
+    paths: [__DIR__ . '/config/xml'],
+    isDevMode: true
+);
+
+$connection = DriverManager::getConnection(
+    [
+        'driver' => 'pdo_mysql',
+        'dbname' => 'candle_grabber',
+        'user' => 'user',
+        'password' => 'example',
+        'host' => 'candle-grabber-db',
+    ],
+    $config
+);
+
+$entityManager = new EntityManager($connection, $config);

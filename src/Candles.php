a4ffef72595de1fac0a252eb1dc1135df211d3b8
Alexandr Krylov <alexandr.krylov@gmail.com>
2024-10-20 13:47:17 +0500

    create table candles

--- src/Candles.php
+++ src/Candles.php
@@ -2,6 +2,7 @@
 
 namespace app;
 
+use DateTime;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
@@ -10,5 +11,38 @@ use Doctrine\Common\Collections\Collection;
 #[ORM\Table(name: 'candles')]
 class Candles
 {
-    
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private int $id;
+
+    #[ORM\Column(type: 'string')]
+    private string $currency;
+
+    #[ORM\Column(type: 'string')]
+    private string $quote_currency;
+
+    #[ORM\Column(type: 'string')]
+    private string $period;
+
+    #[ORM\Column(type: 'datetime')]
+    private DateTime $time;
+
+    #[ORM\Column(type: 'decimal', precision: 20, scale: 14)]
+    private string $open;
+
+    #[ORM\Column(type: 'decimal', precision: 20, scale: 14)]
+    private string $close;
+
+    #[ORM\Column(type: 'decimal', precision: 20, scale: 14)]
+    private string $min;
+
+    #[ORM\Column(type: 'decimal', precision: 20, scale: 14)]
+    private string $max;
+
+    #[ORM\Column(type: 'decimal', precision: 20, scale: 14)]
+    private string $volume;
+
+    #[ORM\Column(type: 'decimal', precision: 20, scale: 14)]
+    private string $volume_quote;
 }

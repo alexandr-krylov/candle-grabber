df41c10fb8761354dced981defe161354d2ebd10
Alexandr Krylov <alexandr.krylov@gmail.com>
2024-10-23 09:10:43 +0500

    it's ready to grab

--- grab_candles.php
+++ grab_candles.php
@@ -3,11 +3,15 @@
 require_once 'bootstrap.php';
 
 use GuzzleHttp\Client;
+use app\Candles;
+use app\Enums\Symbol;
+use app\Enums\Period;
 
 $symbol = $argv[1];
 $fromDate = $argv[2];
 $toDate = $argv[3];
-$period = $argv[4];
+$period = Period::{$argv[4]};
+$limit = 1000;
 
 $startDate = new DateTime($fromDate);
 $endDate = new DateTime($toDate);
@@ -16,15 +20,37 @@ $client = new Client([
     'base_uri' => $_ENV['BASE_URI'],
     'timeout' => 2,
 ]);
-$params = [
-    'query' => [
-        'symbols' => $symbol,
-        'period' => $period,
-        'from' => $startDate->format('Y-m-d\TH:i:s\Z'),
-        'till' => $endDate->format('Y-m-d\TH:i:s\Z'),
-    ]
-];
-$response = $client->get($_ENV['API_PATH'], $params);
-$body = $response->getBody();
-echo $body;
-
+$delta = $endDate->diff($startDate);
+$quantity = ($delta->days * 24 * 60 + $delta->h * 60 + $delta->i) / $period->value;
+while ($quantity > 0) {
+    $params = [
+        'query' => [
+            'symbols' => $symbol,
+            'period' => $period->name,
+            'from' => $startDate->format('Y-m-d\TH:i:s\Z'),
+            'till' => $endDate->format('Y-m-d\TH:i:s\Z'),
+            'limit' => $limit,
+        ]
+    ];
+    $response = $client->get($_ENV['API_PATH'], $params);
+    $body = json_decode((string)($response->getBody()));
+    foreach ($body->$symbol as $data) {
+        $candle = new Candles($data);
+        $candle->setCurrency(Symbol::{$symbol}->currency());
+        $candle->setQuoteCurrency(Symbol::{$symbol}->quoteCurrency());
+        $candle->setPeriod($period);
+        $candle->setTime(new DateTime($data->timestamp));
+        $candle->setOpen($data->open);
+        $candle->setClose($data->close);
+        $candle->setMin($data->min);
+        $candle->setMax($data->max);
+        $candle->setVolume($data->volume);
+        $candle->setVolumeQuote($data->volume_quote);
+        $entityManager->persist($candle);
+        $entityManager->flush();
+    }
+    echo 'till ' . $endDate->format('Y-m-d\TH:i:s\Z') . "\n";
+    $endDate->sub(new DateInterval('PT' . $period->value * $limit . 'M'));
+    $quantity = $quantity - $limit;
+    sleep(2);
+}
--- src/Candles.php
+++ src/Candles.php
@@ -6,6 +6,7 @@ use DateTime;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use app\Enums\Period;
 
 #[ORM\Entity]
 #[ORM\Table(name: 'candles')]
@@ -14,7 +15,7 @@ class Candles
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
-    private int $id;
+    private ?int $id;
 
     #[ORM\Column(type: 'string')]
     private string $currency;
@@ -28,21 +29,67 @@ class Candles
     #[ORM\Column(type: 'datetime')]
     private DateTime $time;
 
-    #[ORM\Column(type: 'decimal', precision: 20, scale: 14)]
+    #[ORM\Column(type: 'decimal', precision: 24, scale: 14)]
     private string $open;
 
-    #[ORM\Column(type: 'decimal', precision: 20, scale: 14)]
+    #[ORM\Column(type: 'decimal', precision: 24, scale: 14)]
     private string $close;
 
-    #[ORM\Column(type: 'decimal', precision: 20, scale: 14)]
+    #[ORM\Column(type: 'decimal', precision: 24, scale: 14)]
     private string $min;
 
-    #[ORM\Column(type: 'decimal', precision: 20, scale: 14)]
+    #[ORM\Column(type: 'decimal', precision: 24, scale: 14)]
     private string $max;
 
-    #[ORM\Column(type: 'decimal', precision: 20, scale: 14)]
+    #[ORM\Column(type: 'decimal', precision: 24, scale: 14)]
     private string $volume;
 
-    #[ORM\Column(type: 'decimal', precision: 20, scale: 14)]
+    #[ORM\Column(type: 'decimal', precision: 24, scale: 14)]
     private string $volume_quote;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setCurrency(string $currency): void
+    {
+        $this->currency = $currency;
+    }
+    public function setQuoteCurrency(string $quote_currency): void
+    {
+        $this->quote_currency = $quote_currency;
+    }
+    public function setPeriod(Period $period): void
+    {
+        $this->period = $period->name;
+    }
+    public function setTime(DateTime $time): void
+    {
+        $this->time = $time;
+    }
+    public function setOpen(string $open): void
+    {
+        $this->open = $open;
+    }
+    public function setClose(string $close): void
+    {
+        $this->close = $close;
+    }
+    public function setMin(string $min): void
+    {
+        $this->min = $min;
+    }
+    public function setMax(string $max): void
+    {
+        $this->max = $max;
+    }
+    public function setVolume(string $volume): void
+    {
+        $this->volume = $volume;
+    }
+    public function setVolumeQuote(string $volume_quote): void
+    {
+        $this->volume_quote = $volume_quote;
+    }
 }

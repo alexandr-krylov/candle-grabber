c10bc13e59f84a5f0831568d234562e0c5a706db
Alexandr Krylov <alexandr.krylov@gmail.com>
2024-12-02 16:09:42 +0500

    add memory_limit. find gaps realized

--- find_gap_in_sequence_candles.php
+++ find_gap_in_sequence_candles.php
@@ -3,28 +3,41 @@
 require_once 'bootstrap.php';
 
 use app\Candle;
+use app\Enums\Period;
+use Doctrine\ORM\Query\Parameter;
+use Doctrine\Common\Collections\ArrayCollection;
 
 $currency = $argv[1];
 $quote_currency = $argv[2];
 $period = $argv[3];
 
-$batchSize = $_ENV['DB_LIMIT'];
-
 $iterator = $entityManager
-    ->getRepository(Candle::class)
-    ->findBy(
-        [
-            'currency' => $currency,
-            'quote_currency' => $quote_currency,
-            'period' => $period,
-        ],
-        [
-            'time' => 'ASC',
-        ]
-        )
+    ->createQueryBuilder()
+    ->select('c')
+    ->from(Candle::class, 'c')
+    ->where('c.currency = :currency')
+    ->andWhere('c.quote_currency = :quote_currency')
+    ->andWhere('c.period = :period')
+    ->orderBy('c.time', 'ASC')
+    ->setParameters(new ArrayCollection([
+            new Parameter('currency', $currency),
+            new Parameter('quote_currency', $quote_currency),
+            new Parameter('period', $period),
+        ])
+    )
+    ->getQuery()
     ->toIterable();
-foreach ($iterator as $candle) {
-    var_dump($candle);
-    break;
-}
+$previous = null;
 
+foreach ($iterator as $candle) {
+    if (null === $previous) {
+        $previous = $candle;
+        continue;
+    }
+    $interval = $previous->getTime()->diff($candle->getTime());
+    if ($interval->i + 60 * $interval->h + 1440 * $interval->d > Period::{$period}->value) {
+        echo "Gap detected! {$previous->getTime()->format('Y-m-d H:i:s')} ---- {$candle->getTime()->format('Y-m-d H:i:s')}\n";
+    }
+    $previous = $candle;
+    $entityManager->detach($candle);
+}

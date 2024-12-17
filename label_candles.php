f7e705d781b77a7d97a1003fcb341414421349c1
Alexandr Krylov <alexandr.krylov@gmail.com>
2024-12-17 23:41:02 +0500

    make random labels in progress

--- label_candles.php
+++ label_candles.php
@@ -5,6 +5,7 @@ require_once 'bootstrap.php';
 use app\Candle;
 use app\Enums\Symbol;
 use app\Enums\Period;
+use Random\Randomizer;
 
 $symbol = Symbol::{$argv[1]};
 $fromDate = $argv[2];
@@ -17,7 +18,17 @@ $limit = $_ENV['DB_LIMIT'];
 
 $startDate = new DateTime($fromDate);
 $endDate = new DateTime($toDate);
+$randomizer = new Randomizer();
 
 for (; $maxQuantity--;) {
     echo "$maxQuantity\n";
+    //make random time
+    $time = date(DATE_ATOM, $randomizer->getInt($startDate->format('U'), $endDate->format('U')));
+    echo "$time\n";
+    $balance = $randomizer->getFloat(0, $maxAmount);
+    echo "$balance\n";
+    echo $randomizer->getInt(0, 4) . "\n";
+    //make random balance quoted currency
+
+    //make random order 1 of 4 times
 }
